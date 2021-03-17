367357f72776c05130d7dde75b54f5379daa048b
Daysha Jones <daysha.vu@icloud.com>
2021-03-17 16:48:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,14 +1,17 @@
 class Song
   attr_accessor :name, :artist, :genre
   
-  @@count = 3
-  @@artist = 0
-  @@genres = 0
+  @@count = 0
+  @@artist = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
+    @@count += 1
+    @@aritsts << artist
+    @@genres << genre
   end
   
   def self.count
